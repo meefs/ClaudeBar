ad07ea29cfc2c54909b3cb0228b8a6d43316a5fc
slamhan <slamhan1987@hotmail.com>
2026-03-15 22:02:14 +0800

    chore(tuist): update SwiftTerm to exact 1.12.0 version
    ci(workflows): simplify build and test steps with tuist commands
    
    - replace xcodebuild with tuist build for debug and release
    - replace xcodebuild test with tuist test, preserving coverage and options
    - remove redundant tuist generate steps before build and test jobs

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -25,7 +25,7 @@ let package = Package(
         // You can read more about dependencies here: https://docs.tuist.io/documentation/tuist/dependencies
         .package(url: "https://github.com/sparkle-project/Sparkle", from: "2.8.1"),
         .package(url: "https://github.com/Kolos65/Mockable.git", from: "0.5.0"),
-        .package(url: "https://github.com/migueldeicaza/SwiftTerm.git", from: "1.5.1"),
+        .package(url: "https://github.com/migueldeicaza/SwiftTerm.git", exact: "1.12.0"),
         .package(url: "https://github.com/awslabs/aws-sdk-swift", from: "1.6.43"),
         .package(url: "https://github.com/steipete/SweetCookieKit.git", from: "0.3.0"),
     ]
